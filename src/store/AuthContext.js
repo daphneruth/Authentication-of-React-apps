1265458a2fa8fd7461dfdd554b5d659e71e82511
Daphne <daphneruth304@gmail.com>
2022-07-24 14:32:45 +0300

    updated

--- src/store/AuthContext.js
+++ src/store/AuthContext.js
@@ -9,5 +9,29 @@ const AuthContext = React.createContext({
 
 const AuthContextProvider = (props) =>{
 
-}
+    const [token, setToken] = useState(null);
+
+    const userIsLoggedIn = !!token;
+  
+    const loginHandler = (token) => {
+      setToken(token);
+    };
+  
+    const logoutHandler = () => {
+      setToken(null);
+    };
+  
+    const contextValue = {
+      token: token,
+      isLoggedIn: userIsLoggedIn,
+      login: loginHandler,
+      logout: logoutHandler,
+    };
+  
+    return (
+      <AuthContext.Provider value={contextValue}>
+        {props.children}
+      </AuthContext.Provider>
+    );
+  };
 export default AuthContext;
