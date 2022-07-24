73964e93a64370b5cc9f2e3ef44729262dc53d3a
Daphne <daphneruth304@gmail.com>
2022-07-24 14:36:16 +0300

    updated

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 import './index.css';
 import App from './App';
+import {Auth}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
