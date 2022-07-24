eb13e8cc0197d7c8615df357bbedbec5a4e3d82d
Daphne <daphneruth304@gmail.com>
2022-07-24 14:41:41 +0300

    updated useContext

--- src/store/AuthContext.js
+++ src/store/AuthContext.js
@@ -1,4 +1,5 @@
-import React,{useState} from 'react';
+import React,{useState, useContext} from 'react';
+import AuthContext from '../../store/AuthContext';
 
 const AuthContext = React.createContext({
   token: '',
